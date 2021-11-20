55f65c01ce8c54ff00df6ff2d19f8dcb17224984
Elko21 <elkodiko@gmail.com>
2021-11-20 13:01:04 +0100

    envoi projet maven

---
+++ Maze/src/test/java/fr/ensem/acl/Maze/AppTest.java
@@ -0,0 +1,20 @@
+package fr.ensem.acl.Maze;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
